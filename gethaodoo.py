08be686e25573a4e3e340ed1a3a678e0740cf417
raylexlee <raylexym57lee@gmail.com>
2021-05-04 00:57:38 +0800

    update entry

--- gethaodoo.py
+++ gethaodoo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# coding: utf-8
 import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -22,21 +22,21 @@ def __GetFromHaoDoo():
    return 0
 
 def GetEpubFromHaoDoo(pLink, code, book ):
-    options = webdriver.ChromeOptions()
-    options.add_argument('user-data-dir=/home/raylex/.config/google-chrome/')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.implicitly_wait(10) # seconds
+    service = Service('/usr/bin/chromedriver')
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+    driver.implicitly_wait(100) # seconds
     driver.get(pLink)
-    submitElement = driver.find_element_by_xpath("//input[@value='下載 epub 檔']")
+    submitElement = driver.find_element_by_xpath('//input[@value="下載 epub 檔"]')
     submitElement.click()
     btn_in_modal_locator = (By.ID, 'okButton')
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 100)
     btn_in_modal = wait.until(EC.element_to_be_clickable(btn_in_modal_locator))
     btn_in_modal.click()
-    file_path = '/home/raylex/epub/japan/' + code + '.epub'
+    file_path = '/home/raylex/Downloads/' + code + '.epub'
     while not os.path.exists(file_path):
         time.sleep(1)
-    os.chdir('/home/raylex/epub/japan')
+    os.chdir('/home/raylex/Downloads')
     os.rename(code + '.epub', book + '.epub')
     driver.quit()
     return
